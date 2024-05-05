db83f18e1bff9b5737366877c49012970d4e322f
Jarrett Jukes <jarrett.jukes@gmail.com>
2024-05-05 16:29:34 -0500

    print out http request

--- code/main.c
+++ code/main.c
@@ -2,7 +2,9 @@
 #include "stdint.h"
 
 #include "sys/socket.h"
-
+#include "netinet/in.h"
+#include "stdio.h"
+#include "arpa/inet.h"
 #define internal static
 #define global static
 #define local static
@@ -23,11 +25,53 @@ int main(int argc, char *argv[])
 {
 	u16 result = 0;
 
-	int tcpSocket = socket(AF_INET, SOCK_STREAM, SOCK_NONBLOCK);
+	int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
 
 	if(tcpSocket != -1)
 	{
+		struct sockaddr_in sockAddress = {0};
+		sockAddress.sin_family = AF_INET;
+		sockAddress.sin_port = htons(8000);
+		sockAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+		//sockAddress.sin_addr.s_addr = inet_aton("192.168.242.130");
+		//inet_aton("192.168.242.130", &sockAddress.sin_addr);
 
+		int bindResult = bind(tcpSocket, (struct sockaddr *)&sockAddress, sizeof(sockAddress));
+		if(bindResult != -1)
+		{
+			int listenResult = listen(tcpSocket, 10);
+			if(!listenResult)
+			{
+				struct sockaddr_in peer;
+				socklen_t peerLen = sizeof(peer);
+				int acceptSocket = accept(tcpSocket, (struct sockaddr *)&peer, &peerLen);
+				if(acceptSocket != -1)
+				{
+					char buffer[4096] = {0};
+					int bytesRead = read(acceptSocket, buffer, sizeof(buffer));
+					if(bytesRead > 0)
+					{
+						printf("%s\n", buffer);
+					}
+					else
+					{
+
+					}
+				}
+				else
+				{
+					//todo(jarrett): logging
+				}
+
+			}
+			else
+			{
+				//todo(jarrett): logging
+			}
+		}
+		else
+		{
+		}
 	}
 	else
 	{
